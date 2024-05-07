65427290367de4b526c4cf8de01e469df118161b
khushigangwar <khushi@videalpha.com>
2024-05-07 11:25:27 +0530

    Add project files.

---
+++ Controllers/UploaderController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using saveimage.Model;
+
+namespace saveimage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploaderController : ControllerBase
+    {
+        [HttpPost]
+        [Route("uploadfiles")]
+        public Response UploadFile([FromForm] FileModel fileModel)
+        {
+            Response response = new Response();
+            try
+            {
+                string path = Path.Combine(@"C:\Users\VideAlpha\Pictures\my images", fileModel.FileName);
+                using (Stream stream=new FileStream(path, FileMode.Create))
+                {
+                    fileModel.file.CopyTo(stream);
+                }
+                response .StatusCode = 200;
+                response.Message = "Image created Successfully";
+
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = 100;
+                response.Message = ex.Message;
+            }
+            return response;
+        }
+    }
+}
